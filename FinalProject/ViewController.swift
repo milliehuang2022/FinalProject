8ae540ba3ad15639e62b953f6b8795abc226d25b
Millie Huang <milliehuang@millies-macbook-air.local>
2021-07-20 13:53:48 -0700

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Millie Huang on 7/20/21.
+//  Copyright © 2021 Millie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
