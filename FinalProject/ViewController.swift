49999ac0092da5ec9874e3d7dcf1078adf4c5df4
milliehuang2022 <milliehuang2022@gmail.com>
2021-07-21 11:49:31 -0700

    added buttons and labels to all four view controllers

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
